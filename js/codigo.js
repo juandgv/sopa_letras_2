21192778b8a65261f2e2c082ba931a85c7e7f9f1
juandgv <juandagilva85@gmail.com>
2021-05-26 23:49:01 -0500

    Sopa de letras 2

---
+++ js/codigo.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+ $(".java").hover(function(){
+    $(".java").addClass('resaltar');
+ });
+
+ $(".java").mouseleave(function(){
+     $(".java").removeClass('resaltar');
+ })
+
+ $(".codigo").hover(function(){
+    $(".codigo").addClass('resaltar')
+ });
+
+ $(".codigo").mouseleave(function(){
+     $(".codigo").removeClass('resaltar')
+ })
+
+ $(".app").hover(function(){
+    $(".app").addClass('resaltar')
+ });
+
+ $(".app").mouseleave(function(){
+     $(".app").removeClass('resaltar')
+ })
+
+ $(".html").hover(function(){
+    $(".html").addClass('resaltar')
+ });
+
+ $(".html").mouseleave(function(){
+     $(".html").removeClass('resaltar')
+ })
+ $(".movil").hover(function(){
+    $(".movil").addClass('resaltar')
+ });
+
+ $(".movil").mouseleave(function(){
+     $(".movil").removeClass('resaltar')
+ })
+ $(".css").hover(function(){
+    $(".css").addClass('resaltar')
+ });
+
+ $(".css").mouseleave(function(){
+     $(".css").removeClass('resaltar')
+ })
+ $(".bug").hover(function(){
+    $(".bug").addClass('resaltar')
+ });
+
+ $(".bug").mouseleave(function(){
+     $(".bug").removeClass('resaltar')
+ })
+ $(".host").hover(function(){
+    $(".host").addClass('resaltar')
+ });
+
+ $(".host").mouseleave(function(){
+     $(".host").removeClass('resaltar')
+ })
+
+
+});
